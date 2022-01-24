6dd111047c2b7dd730ae625146b55d33e5e60409
LuisilloElProgramador <56002200+LuisilloElProgramador@users.noreply.github.com>
2022-01-23 21:20:09 -0700

    Avances

--- Fundations/Prueba-1.py
+++ Fundations/Prueba-1.py
@@ -1,1 +1,36 @@
-print("Hola mundo")
+import collections
+import math
+from typing import Any, DefaultDict, List, Set, Tuple
+
+############################################################
+# Custom Types
+# NOTE: You do not need to modify these.
+
+"""
+You can think of the keys of the defaultdict as representing the positions in
+the sparse vector, while the values represent the elements at those positions.
+Any key which is absent from the dict means that that element in the sparse
+vector is absent (is zero).
+Note that the type of the key used should not affect the algorithm. You can
+imagine the keys to be integer indices (e.g., 0, 1, 2) in the sparse vectors,
+but it should work the same way with arbitrary keys (e.g., "red", "blue", 
+"green").
+"""
+SparseVector = DefaultDict[Any, float]
+Position = Tuple[int, int]
+def increment_sparse_vector(v1: SparseVector, scale: float, v2: SparseVector,
+) -> None:
+    """
+    Given two sparse vectors |v1| and |v2|, perform v1 += scale * v2.
+    If the scale is zero, you are allowed to modify v1 to include any
+    additional keys in v2, or just not add the new keys at all.
+
+    NOTE: This function should MODIFY v1 in-place, but not return it.
+    Do not modify v2 in your implementation.
+    This function will be useful later for linear classifiers.
+    """
+    # BEGIN_YOUR_CODE (our solution is 2 lines of code, but don't worry if you deviate from this)
+    for llaves in v2.keys() :
+        v1[llaves] += scale * v2[llaves]
+
+SparseVector 
