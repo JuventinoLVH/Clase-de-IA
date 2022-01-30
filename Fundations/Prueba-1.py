2f1ca525e5df96db13158a136c5d3cbef13b053b
LuisilloElProgramador <56002200+LuisilloElProgramador@users.noreply.github.com>
2022-01-30 10:27:08 -0700

    Finish

--- Fundations/Prueba-1.py
+++ Fundations/Prueba-1.py
@@ -1,23 +1,8 @@
 import collections
 import math
 from typing import Any, DefaultDict, List, Set, Tuple
-
-############################################################
-# Custom Types
-# NOTE: You do not need to modify these.
-
-"""
-You can think of the keys of the defaultdict as representing the positions in
-the sparse vector, while the values represent the elements at those positions.
-Any key which is absent from the dict means that that element in the sparse
-vector is absent (is zero).
-Note that the type of the key used should not affect the algorithm. You can
-imagine the keys to be integer indices (e.g., 0, 1, 2) in the sparse vectors,
-but it should work the same way with arbitrary keys (e.g., "red", "blue", 
-"green").
-"""
 SparseVector = DefaultDict[Any, float]
-Position = Tuple[int, int]
+
 def increment_sparse_vector(v1: SparseVector, scale: float, v2: SparseVector,
 ) -> None:
     """
@@ -31,6 +16,10 @@ def increment_sparse_vector(v1: SparseVector, scale: float, v2: SparseVector,
     """
     # BEGIN_YOUR_CODE (our solution is 2 lines of code, but don't worry if you deviate from this)
     for llaves in v2.keys() :
-        v1[llaves] += scale * v2[llaves]
+        if(v2[llaves] != 0):
+           v1[llaves] += scale * v2[llaves]
 
-SparseVector 
+    # END_YOUR_CODE
+
+increment_sparse_vector({1:32,4:32,15:3} , 3 ,{1:32,4:32,15:3} )
+
